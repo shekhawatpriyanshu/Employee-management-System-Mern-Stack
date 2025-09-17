723e845737bb6a8582eeaaed1d734d5ba931486c
shekhawatpriyanshu <146126147+shekhawatpriyanshu@users.noreply.github.com>
2025-09-17 19:19:32 +0530

    Update Table.jsx

--- frontend/src/components/leave/Table.jsx
+++ frontend/src/components/leave/Table.jsx
@@ -9,7 +9,7 @@ const Table = () => {
 
   const fetchLeaves = async () => {
     try {
-      const response = await axios.get("http://localhost:5000/api/leave", {
+      const response = await axios.get("https://employee-management-system-mern-sta-ten.vercel.app/api/leave", {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
         },

3e3a6913f2830b90449674a9280229ee0b45c942
shekhawatpriyanshu <146126147+shekhawatpriyanshu@users.noreply.github.com>
2025-09-17 19:13:51 +0530

    Update Setting.jsx

--- frontend/src/components/Employeedashboard/Setting.jsx
+++ frontend/src/components/Employeedashboard/Setting.jsx
@@ -25,7 +25,7 @@ const Setting = () => {
         }
         else{
             try {
-                const response=await axios.put("http://localhost:5000/api/setting/change-password",
+                const response=await axios.put("https://employee-management-system-mern-sta-ten.vercel.app/api/setting/change-password",
                 setting,{
                     headers:{
                        Authorization:`Bearer ${localStorage.getItem('token')}`

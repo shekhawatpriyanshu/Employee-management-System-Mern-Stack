b3d4ae82995727ea0cf6235a95dd3574c7900730
shekhawatpriyanshu <146126147+shekhawatpriyanshu@users.noreply.github.com>
2025-09-17 19:04:07 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,12 @@ import leaveRouter from'./routes/leave.js'
 dotenv.config()
 const port=process.env.PORT || 3000;
 const app=express();
-app.use(cors())
+app.use(cors(
+    {
+    origin: "https://employee-management-system-mern-sta.vercel.app/",
+    credentials: true,
+  }
+))
 app.use(express.json()) 
 app.use(express.static('public/uploads'))
 app.use('/api/auth',authRouter)

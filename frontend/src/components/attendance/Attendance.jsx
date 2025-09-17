fd2f9035e31488c8f3831a93fda2f76ded34b7bb
shekhawatpriyanshu <146126147+shekhawatpriyanshu@users.noreply.github.com>
2025-09-17 19:14:53 +0530

    Update Attendance.jsx

--- frontend/src/components/attendance/Attendance.jsx
+++ frontend/src/components/attendance/Attendance.jsx
@@ -16,7 +16,7 @@ const Attendance= () => {
    const fetchAttendance = async () => {
      setLoading(true);
      try {
-       const response = await axios.get("http://localhost:5000/api/attendance", {
+       const response = await axios.get("https://employee-management-system-mern-sta-ten.vercel.app/api/attendance", {
          headers: {
            Authorization: `Bearer ${localStorage.getItem("token")}`,
          },
